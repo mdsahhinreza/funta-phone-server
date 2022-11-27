7a2f83772365e66e520f96b2a8da82f5e5523a50
mdsahhinreza <mdsahhinreza@gmail.com>
2022-11-27 21:19:10 +0600

    Server Ready and User Post Api added

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.epiqiul.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+console.log(uri);
+
+async function run() {
+  try {
+    const userCollection = client.db("funtaPhone").collection("users");
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.log);
+
+app.get("/", (req, res) => {
+  res.send("Funta Phone Server is Runnig");
+});
+
+app.listen(port, () => {
+  console.log(`Funta Phone Server is Running on ${port}`);
+});
